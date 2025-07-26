55d05f4de9a303b00276a843e5a61d5e4265a69f
caadan992 <caadan992@gmail.com>
2025-07-26 22:25:14 +0800

    voting page

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return(
     <Routes>
       <Route path="/" element ={< LogIn/>} />
-      <Route path="/Dash" element={<Dashboard/>}/>
+      <Route path="/Dashboard" element={<Dashboard/>}/>
     </Routes>
   )
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,80 @@
+import "./css/Dashboard.css"
 
-
-export default function (){
+ function Dashboard (){
     return(
-    <>
-    <header/>
-    <h2>HI</h2>
-    </>
+        <div className="bodies">
+   <header className = "navbar">
+    <nav className="navbar-links">
+    <ul><li><a href="#">Dashboard</a></li>
+        <li><a href="#">Party Lists</a></li>
+        <li><a href="#">Results</a></li>
+        <li><a href="#">Contacts</a></li>
+        </ul>
+        </nav>
+   </header>
+
+ <section class="partylists">
+
+      <div class="partylist-card">
+        <h2>Partylist:</h2>
+        <form action="#" method="POST">
+
+          <div class="member">
+            <span>President:</span>
+            <select name="auditor">
+              <option value="">Select Candidate</option>
+           
+            </select>
+          </div>
+          <div class="member">
+               <span>Vice President:</span>
+           <select name="auditor">
+              <option value="">Select Candidate</option>
+           
+            </select>
+          </div>
+
+          <div class="member">
+            <span>Secretary:</span>
+             <select name="auditor">
+              <option value="">Select Candidate</option>
+           
+            </select>
+          </div>
+
+          <div class="member">
+            <span>Treasurer:</span>
+             <select name="auditor">
+              <option value="">Select Candidate</option>
+           
+            </select>
+          </div>
+
+          <div class="member">
+            <span>Auditor:</span>
+            <select name="auditor">
+              <option value="">Select Candidate</option>
+           
+            </select>
+          </div>
+
+          <div class="member">
+            <span>PRO:</span>
+            <select name="pro">
+              <option value="">Select Candidate</option>
+             
+            </select>
+          </div>
+
+          <div class="submit-container">
+            <button type="submit" class="submit-btn">Submit Vote</button>
+          </div>
+        </form>
+      </div>
+
+    </section>
+
+</div>
+
     )
-}
+} export default Dashboard;
