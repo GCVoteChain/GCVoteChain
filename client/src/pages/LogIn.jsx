7e35abe84f814004a1e6691577f91067a5538f83
caadan992 <caadan992@gmail.com>
2025-08-02 00:07:18 +0800

    login process on progress

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,7 +1,32 @@
 import "./css/Login.css"
+import { useState } from "react"
 
 
 function Login (){
+    const [user,setuser] = useState ("")
+    const [pass,setpass] = useState ("")
+    const changeuser = event => {
+        setuser (event.target.value)
+    }
+
+    const changepass = event => {
+        setpass (event.target.value)
+    }
+
+    const enter =  () => {
+        fetch(
+            'http://localhost:8008/api/auth/login',
+            {
+                method:'POST',
+                headers:{'Content-Type': 'application/json' },
+                body: JSON.stringify({ studentId: user , password: pass})
+            }
+            
+        )
+        .catch(err=> console.error('Erro loging in',err.message));
+         
+    }
+
     return(
         <div className="body">
             <div className="wrapper">
@@ -9,10 +34,10 @@ function Login (){
                 <form action="">
                     <h1>Login</h1>
                     <div className="input-box">
-                        <input type="text" placeholder="Username" required/>
+                        <input type="text" placeholder="Username" onChange={changeuser} value={user} required/>
                     </div>
                     <div className="input-box">
-                        <input type="Password" placeholder="Password" required/>
+                        <input type="Password" placeholder="Password" onChange={changepass} value={pass} required/>
                     </div>
                    <div className="Button"><button type="submit" onClick={enter()} >Login</button></div>
                 </form>
@@ -20,16 +45,5 @@ function Login (){
         </div>
     )
 
-}
-function enter (){
-fetch(
-    '/login',
-{
-    method: 'POST',
-    headers: {'Content-Type' : 'application/json'},
-    body: JSON.stringify({})
-}
-
-)
 }
 export default Login;
