a67e1b6fc83084f9a58dab43b585a56f067bc6a0
djsahagun15 <179677237+djsahagun15@users.noreply.github.com>
2025-08-05 11:43:00 +0800

    Update routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,37 @@
 import {  Routes,Route } from "react-router-dom";
 import LogIn  from "./pages/LogIn"
+import Result from "./pages/Result";
+import Settings from "./pages/Settings";
+
 import Student from "./pages/Student/Student";
+import StudentCandidate from "./pages/Student/Candidates";
+import StudentElection from "./pages/Student/Election";
+import Voting from "./pages/Student/Voting";
+
 import Admin from "./pages/Admin/Admin";
-import Result from "./pages/Student/Result";
-import PartyList from "./pages/Student/PartyList";
-import Candidate from "./pages/Admin/Candidate";
-import Registration from "./pages/Admin/Registration";
-import Voting from "./pages/Admin/Voting"
-import Settings from "./pages/Student/Settings";
+import VoterRegistration from "./pages/Admin/Registration";
+import AdminCandidate from "./pages/Admin/Candidates";
+import AdminElection from "./pages/Admin/Election";
+
 
 function App() {
   return(
     <Routes>
       <Route path="/" element ={< LogIn/>} />
-      <Route path="/Student" element={<Student/>}/>
-      <Route path="/Result" element={<Result/>}/>
-      <Route path="/PartyList" element= {<PartyList/>}/>
-      <Route path="/Admin" element={<Admin/>}/>
-      <Route path="/Voting" element={<Voting/>}/>
-      <Route path="/Settings" element={<Settings/>}/>
-      <Route path="/Registration" element={<Registration/>}/>
-      <Route path="/Candidate" element={<Candidate/>}/>
+      <Route path="/student" element={<Student/>}/>
+      <Route path="/student/candidates" element= {<StudentCandidate/>}/>
+      <Route path="/student/election" element={<StudentElection/>}/>
+      <Route path="/student/vote" element={<Voting/>}/>
       
+      <Route path="/admin" element={<Admin/>}/>
+      <Route path="/admin/registration" element={<VoterRegistration/>}/>
+      <Route path="/admin/candidates" element={<AdminCandidate/>}/>
+      <Route path="/admin/election" element={<AdminElection/>}/>
+
+      <Route path="/settings" element={<Settings/>}/>
+      <Route path="/results" element={<Result/>}/>
     </Routes> 
   )
-
-
-  
 }
 
 
