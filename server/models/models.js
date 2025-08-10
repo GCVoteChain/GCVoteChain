be4c0383c409990a1801195f0776c683563dc423
djsahagun15 <179677237+djsahagun15@users.noreply.github.com>
2025-08-10 23:01:48 +0800

    Remove references to vote components

--- server/models/models.js
+++ server/models/models.js
@@ -1,7 +1,6 @@
 const user = require('./userModel.js');
 const candidate = require('./candidateModel.js');
 const election = require('./electionModel.js');
-const vote = require('./voteModel.js');
 
 function finalize() {
     user.getUserStmt.finalize();
@@ -18,16 +17,12 @@ function finalize() {
     election.removeElectionStmt.finalize();
     election.setElectionScheduleStmt.finalize();
     election.setElectionStatusStmt.finalize();
-    
-    vote.addVoteStmt.finalize();
-    vote.getVoteStmt.finalize();
 }
 
 module.exports = {
     user,
     candidate,
     election,
-    vote,
 
     finalize
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const authRoutes = require('./authRoutes');
 const candidateRoutes = require('./candidateRoutes');
 const electionRoutes = require('./electionRoutes');
-const voteRoutes = require('./voteRoutes');
 
 const router = express.Router();
 
 router.use('/api/auth', authRoutes);
 router.use('/api/candidates', candidateRoutes);
 router.use('/api/elections', electionRoutes);
-router.use('/api/votes', voteRoutes);
 
 module.exports = router;
